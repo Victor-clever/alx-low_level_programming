c974df04fbb4dcd60754c9068b35f5a4bac4004f
Okoli victor chimdinyerem <victorclever133@gmail.com>
2023-06-30 19:10:48 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putcahr('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putcahr((n1 % 10) + '0');
+}
+
