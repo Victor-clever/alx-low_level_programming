38fc1baedf37a5a8aadb9c3183a8728b73d4b984
Okoli victor chimdinyerem <victorclever133@gmail.com>
2023-06-30 20:01:47 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,13 +14,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putcahr('-');
+		_putchar('-');
 		n1 = -n;
 	}
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-	_putcahr((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
 
