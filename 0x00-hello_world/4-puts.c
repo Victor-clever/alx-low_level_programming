a2520e3083f0ed33f5269e45396b6e0226958e71
Okoli Victor <victorclever133@gmail.com>
2023-08-10 09:33:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
+
 /**
  * main - A program that prints a line with puts function
- * Return: 0 (sucess)
+ * return 0.
  */
-int main(void) 
+int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
