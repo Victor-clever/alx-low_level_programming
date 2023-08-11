050669513730e75ab25cc4a2253a820499eec5ef
Okoli Victor <victorclever133@gmail.com>
2023-08-11 16:35:02 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 /**
- * main - Prints " A program that prints a line using printf funtion
- * a piece of art, " followed by a new line.
- *
- * Return: Always 0.
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
